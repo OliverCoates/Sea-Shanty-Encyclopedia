77caf2279c0a0c18f6587accbd8ba7a4a4e2df69
Oliver C <17012@burnside.school.nz>
2020-04-24 11:55:43 +1200

    Python file can now interact with the SQL database through SQLAlchemy

--- SHE.py
+++ SHE.py
@@ -1,3 +1,22 @@
-print("Hello World!")
+import os
+from flask import Flask
+from flask  import render_template
+from flask import request
+from flask import redirect
+from flask_sqlalchemy import SQLAlchemy
 
-print("This is a second test!")
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "SeaShantyDatabase.db"))
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class SeaShanty(db.Model):
+    ID = db.Column(db.Integer(), primary_key=True,)
+    Name = db.Column(db.String(),unique=True)
+
+print("")
+print("Database Sucessfully loaded.")
+print("")
