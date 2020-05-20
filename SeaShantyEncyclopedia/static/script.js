a0564490c6776e0ae7f0a6d976ef9702a113b21b
Oliver C <17012@burnside.school.nz>
2020-05-21 11:41:56 +1200

    Admin login system now working

--- SeaShantyEncyclopedia/static/script.js
+++ SeaShantyEncyclopedia/static/script.js
@@ -1,5 +1,28 @@
 //query select all
 
+window.onkeyup = keyup;  // Listener for when a key is pressed
+
+var passwordTextValue;
+
+function keyup(e) {
+  passwordTextValue = e.target.value;
+  if (e.keyCode == 13 && passwordTextValue == "Password")  { // Check for when enter pressed
+    //window.alert("Hey!");
+    e.target.value = ""; // Reset what is in the password field
+    var panel = document.getElementById("AdminPanel");
+    var input = document.getElementById("Password");
+    panel.style.display = 'block';
+    input.style.display = 'none';
+  }
+}
+
+function logout() {
+  var panel = document.getElementById("AdminPanel");
+  var input = document.getElementById("Password");
+  panel.style.display = 'none';
+  input.style.display = 'block';
+}
+
 function dropdown(elementID) {
   var boxID = document.getElementById(elementID);
   //window.alert("Hello");
